7899840ad8c05c529036d3bb0c865481c54fdb17
Artemitol <artemiy.averin@gmail.com>
2024-12-29 02:05:29 +0300

    добавил докер для фронта и переделал docker compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,9 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+        server: {
+            port: 5173,
+        },
         plugins: [react()],
         resolve: {
             alias: {
